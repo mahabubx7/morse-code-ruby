2822c336a7eac200cce89e325d6c7d639557f1f5
Lawrence Amoafo <rekenburg777@gmail.com>
2023-06-14 14:47:36 +0000

    Add extra tests to test the morse code functions

--- spec/morse_core_spec.rb
+++ spec/morse_core_spec.rb
@@ -4,8 +4,11 @@ require_relative '../morse_code'
 
 describe 'Unit tests for morse code to string' do
   it 'returns morse code to letter' do
-    expect(decord_word('.-')).to eq('A')
-    expect(decord_word('-- .- .... .- -... ..- -...')).to eq('MAHABUB')
-    expect(decord_word('.-.. .- .-- .-. . -. -.-. .')).to eq('LAWRENCE')
+    expect(decode_char('.-')).to eq('A')
+  end
+
+  it 'returns morse code to word' do
+    expect(decode_word('-- .- .... .- -... ..- -...')).to eq('MAHABUB')
+    expect(decode_word('.-.. .- .-- .-. . -. -.-. .')).to eq('LAWRENCE')
   end
 end
